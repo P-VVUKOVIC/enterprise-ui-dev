465f9dc47611f583bab5a2a0f987cf7f51d287fb
Valentina Vuković <valvkvc@gmail.com>
2024-11-14 11:57:33 +0100

    update config

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,5 +1,6 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// @vitest-environment happy-dom
+
+import { screen, render } from './test/utilities';
 import Counter from '.';
 
 test('it should render the component', () => {
@@ -9,8 +10,7 @@ test('it should render the component', () => {
 });
 
 test('it should increment when the "Increment" button is pressed', async () => {
-  const user = userEvent.setup();
-  render(<Counter />);
+  const { user } = render(<Counter />);
 
   const currentCount = screen.getByTestId('current-count');
   const incrementButton = screen.getByRole('button', { name: 'Increment' });
@@ -20,9 +20,23 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={4000} />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('4000');
+});
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  const { user } = render(<Counter initialCount={65} />);
+
+  const currentCount = screen.getByTestId('current-count');
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+  const resetButton = screen.getByRole('button', { name: 'Reset' });
+
+  await user.click(incrementButton);
+  await user.click(incrementButton);
+  expect(currentCount).toHaveTextContent('67');
+
+  await user.click(resetButton);
+  expect(currentCount).toHaveTextContent('0');
+});
